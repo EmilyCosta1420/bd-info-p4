71ba89276682077bae0d0bffa26e20c99efff83b
Emily Costa <113480636+EmilyCosta1420@users.noreply.github.com>
2023-06-23 16:17:42 -0300

    Add files via upload

---
+++ Avaliacoes/avaliacao 10/avaliacao_10/migrations/20230623174914_migration_name.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('tb_alunos', function(table) {
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.string('email').notNullable().unique();
+      })
+      .createTable('tb_professores', function(table) {
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.string('email').notNullable().unique();
+      })
+      .createTable('tb_disciplinas', function(table) {
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.integer('professor_id').unsigned().references('id').inTable('professores');
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('tb_disciplinas')
+      .dropTableIfExists('tb_alunos')
+      .dropTableIfExists('tb_professores');
+  };
